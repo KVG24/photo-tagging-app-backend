7d8c5dbc762ba331d197265ee8dce8283d5294d0
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-11-15 07:13:14 +0000

    Add get characters route, db query and controller
    Remove controllers folder

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,11 @@
 const { PrismaClient } = require("../generated/prisma");
 
 const prisma = new PrismaClient();
+
+async function getCharacters() {
+    return await prisma.character.findMany();
+}
+
+module.exports = {
+    getCharacters,
+};
--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,15 @@
 const { Router } = require("express");
 const router = Router();
+const db = require("../db/queries");
+
+router.get("/characters", async (req, res) => {
+    try {
+        const characters = await db.getCharacters();
+        res.status(200).json(characters);
+    } catch (err) {
+        console.error("Error fetching characters data: " + err);
+        res.status(500).json({ error: "Error fetching characters" });
+    }
+});
 
 module.exports = router;
